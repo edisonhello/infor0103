e2006901408bbd33165a9b04e92286654fffb1a5
edisonhello <edisonhello@hotmail.com>
2015-12-24 22:49:50 +0800

    fixing db

--- node/app.js
+++ node/app.js
@@ -2,6 +2,8 @@
 
 var path = require('path');
 var express = require('express');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
 var bodyParser = require('body-parser');
 
 var app=express();
@@ -14,7 +16,12 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html',function(){res.end();})
 });
 
-app.post('/',function(req,res){
+app.get('/login.html',function(req,res){
+  res.sendFile(__dirname+'/login.html',function(){res.end();})
+});
+
+//app.get('/login', function(){})
+app.post('/reg',function(req,res){
   console.log("got post");
 	if(req.body.pass!=req.body.pass2){
     console.log("pass q");
@@ -23,18 +30,28 @@ app.post('/',function(req,res){
   else{
     if(req.body.birth==""){
       console.log("bir q");
-        res.sendFile(__dirname+'/form_birth.html')
-        res.end();
-       }else{
-      console.log("no q");
-      res.sendFile(__dirname+'/done.html')
-      res.end();
-       }
-    }
+      res.sendFile(__dirname+'/form_birth.html',function(){res.end();})
+   }else{
+     console.log("no f q");
+     MongoClient.connect('mongodb://127.0.0.1:27017/users',function(err,db){
+      console.log(req.body);
+      db.collection('users').insertOne(req.body);
+      console.log('no db q');
+    });
+     res.sendFile(__dirname+'/done.html',function(){res.end();})
+   }
+  }
 });
 
-app.get('/list',function(req,res){
-//    res.sendFile(__dirname+'/list.html',function(){res.end();})
+app.post('/login',function(req,res){
+  console.log('get login q');
+  console.log(req.body.username);
+  MongoClient.connect('mongodb://127.0.0.1:27017/users',function(err,db){
+    console.log(db.collection('users').find({"username":req.body.username}).count());
+    console.log(db.collection('users').find().count());
+
+  });
+ // if(db.users.findOne(req.body.username))
 });
 
 app.listen(3000);
