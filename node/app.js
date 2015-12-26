a05028f41c47fb3e785300d84fa4d42324cdb9c4
edisonhello <edisonhello@hotmail.com>
2015-12-27 01:50:03 +0800

    new reg

--- node/app.js
+++ node/app.js
@@ -37,49 +37,8 @@ app.get('/login.html',function(req,res){
   res.sendFile(__dirname+'/login.html',function(){res.end();})
 });
 
-//app.get('/login', function(){})
-app.post('/reg',function(req,res){
-  console.log("got post");
-	if(req.body.pass!=req.body.pass2){
-    console.log("pass q");
-		res.sendFile(__dirname+'/form_pass.html',function(){res.end();})
-  }
-  else{
-    if(req.body.birth==""){
-      console.log("bir q");
-      res.sendFile(__dirname+'/form_birth.html',function(){res.end();})
-   }else{
-     console.log("no f q");
-     MongoClient.connect('mongodb://127.0.0.1:27017/users',function(err,db){
-      console.log(req.body);
-      db.collection('users').insertOne(req.body);
-      console.log('no db q');
-    });
-     res.sendFile(__dirname+'/done.html',function(){res.end();})
-   }
-  }
-});
-
-app.post('/login',function(req,res){
-  console.log('get login q');
-  console.log(req.body.username);
-  MongoClient.connect('mongodb://127.0.0.1:27017/users',function(err,db){
-    console.log(db.collection('users').find({"username":req.body.username}).count(function(err,cnt){
-      console.log(cnt);
-      if(cnt==0){res.sendFile(__dirname+'/login_username.html',function(){res.end();})}
-      else{
-        db.collection('users').find({"pass":req.body.pass}).count(function(err,cnt2){
-          if(cnt2==0){res.sendFile(__dirname+'/login_pass.html',function(){res.end();})}
-          else if(cnt2==1){
-            res.sendFile(__dirname+"/chatroom.html",function(){res.end();})
-          }  
-        });
-      }
-    }));
-    console.log(db.collection('users').find().count());
-
-  });
- // if(db.users.findOne(req.body.username))
+app.post('/gochat',function(req,res){
+  res.sendFile(__dirname+'/chatroom.html',function(){res.end();})
 });
 
 app.post('/message_input',function(req,res){
@@ -99,9 +58,15 @@ io.sockets.on('connection', function(socket){
   });
 
   socket.on('sendchat', function(text,name){
-    var time=Math.floor(new Date().getTime()/1000)
+    var now = new Date().getFullYear().toString()+"-"+new Date().getMonth().toString()+"-"+new Date().getDate().toString()+" ";
+    if(new Date().getHours()<10){var nnow=now+"0"+new Date().getHours().toString()+":";}
+      else{var nnow=now+new Date().getHours().toString()+":";}
+    if(new Date().getMinutes()<10){var nnnow=nnow+"0"+new Date().getMinutes().toString()+":";}
+      else{var nnnow=nnow+new Date().getMinutes().toString()+":";}
+    if(new Date().getSeconds()<10){var nnnnow=nnnow+"0"+new Date().getSeconds().toString();}
+      else{var nnnnow=nnnow+new Date().getSeconds().toString();}
     console.log(text+" "+name+" "+time);
-    io.emit("pubchat", text, name ,time);
+    io.emit("pubchat", text, name ,nnnnow);
   });
 
 
@@ -115,12 +80,26 @@ io.sockets.on('connection', function(socket){
       else{var nnnnow=nnnow+new Date().getSeconds().toString();}
     socket.emit('now', {'date':nnnnow});
   }, 1000);
+
+  socket.on('regq',function(username,pass,pass2,email,birth){
+    MongoClient.connect('mongodb://127.0.0.1:27017/users',function(err,db){
+      var cnt=db.collection('users').find({"username":username}).count(function(cnt){
+        if(cnt){io.emit('usernameq');}
+        else{
+           db.collection('users').insertOne({"username":username,"pass":pass,"pass2":pass2,"email":email,"birth":birth});
+           io.emit('jumpmain');
+        }
+      });
+    });
+  });
+
 });
 
-
-
-app.get("/socket.io/socket.io.js",function(req,res){
-  res.sendFile(__dirname+'/node_modules/socket.io-client/socket.io.js',function(){res.end();})
+MongoClient.connect('mongodb://127.0.0.1:27017/users',function(err,db){
+  db.collection('users').remove(function(){
+    db.collection('users').insertOne({"username":"a","pass":"b"})
+  });
 });
+
 
 server.listen(3000);
