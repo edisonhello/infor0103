899e28138aefa15803d63976bed9872ebb379087
edisonhello <edisonhello@hotmail.com>
2015-12-26 17:11:31 +0800

    20151226

--- meteor/meteor.js
+++ meteor/meteor.js
@@ -1,5 +1,15 @@
-if (Meteor.isClient) {
+message_collection = new Mongo.Collection('messages');
 
+if (Meteor.isClient) {
+  Template.body.events({
+    "submit .message-form":function(event){
+      var message=event.target.message.value;
+console.log(message);
+      message_collection.insert({"message":message});
+      
+      event.target.message.value="";
+    }
+  });
 }
 
 if (Meteor.isServer) {
